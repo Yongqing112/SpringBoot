fb4ffb102d73401d9f1208714e0207861d34dfca
蕭永清 <ching8989889@gmail.com>
2024-07-12 20:07:57 +0800

    Install environment

---
+++ src/main/java/com/hello/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/name")
+    public String getName(){
+        return "hello, World!";
+    }
+}
